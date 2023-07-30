176631a61ff2c915e4d26ac767be39c1eda6659a
Anton Malov <maloanto@yandex.ru>
2023-07-30 19:59:18 +0300

    added clarifications

--- src/test/java/demoqa/tests/cssXpathExamples.java
+++ src/test/java/demoqa/tests/cssXpathExamples.java
@@ -7,6 +7,7 @@ import static com.codeborne.selenide.Selenide.$x;
 public class cssXpathExamples {
 
     void cssXpathExample() {
+        //поиск по атрубуту data-testid="email"
         // <input type="email" class="inputtext" login_form_input_box" name="email" id="email" data-testid="email">
         $("input[data-testid=email]").setValue("1");
         $("[data-testid=email]").setValue("1");
@@ -14,21 +15,26 @@ public class cssXpathExamples {
         $x("//*[@]data-testid='email']").setValue("1");
         $(byXpath("//*[@]data-testid='email']")).setValue("1");
 
+        //поиск по id
         // <input type="email" class="inputtext" login_form_input_box" name="email" id="email">
         $("[id=email]").setValue("1");
         $("[#email]").setValue("1");
         $("[input#email]").setValue("1");
 
+        //поиск по имени
         // <input type="email" class="inputtext" login_form_input_box" name="email">
         $("[name=email]").setValue("1");
         $(byName("email")).setValue("1");
 
+        // поиск по классу
         // <input type="email" class="inputtext" login_form_input_box">
         $("[class=login_form_input_box]").setValue("1");
         $(".login_form_input_box").setValue("1");
         $(".inputtext.login_form_input_box").setValue("1");
         $("[input.inputtext.login_form_input_box]").setValue("1");
 
+
+        //поиск вложенного локатора
         // <div class="inputtext">
         //  <input type="email" class="login_form_input_box">
         // </div>
@@ -37,9 +43,13 @@ public class cssXpathExamples {
         $("div.inputtext input.login_form_input_box").setValue("1");
         $("div.inputtext").$("input.login_form_input_box").setValue("1");
 
+
+        //поиск по тексту
         //<div>Hello, qa.guru!</div>
         $x("//div[text()='Hello, qa.guru!']").click();
         $(byText("Hello, qa.guru!")).click();
         $(withText("lo, qa.guru!")).click();
+
+
     }
 }
